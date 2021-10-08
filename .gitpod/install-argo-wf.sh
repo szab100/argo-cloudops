cda3888de406f32d2b4930e6e167c153f44cb2b7
Szabolcs Fruhwald <git@szab100.com>
2021-10-08 23:30:06 +0000

    increase sleep before installing argo

--- .gitpod/install-argo-wf.sh
+++ .gitpod/install-argo-wf.sh
@@ -8,7 +8,7 @@ if test -f "${argoreadylock}"; then
 fi
 
 echo "🔥 Installing Argo Workflows into local K3S cluster.."
-sleep 10
+sleep 15
 kubectl create namespace argo
 kubectl apply -n argo -f https://github.com/argoproj/argo-workflows/releases/download/v3.1.13/install.yaml
 

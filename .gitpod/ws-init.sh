6e5a1d22de7233f143291e823253e42663f0e0e6
Szabolcs Fruhwald <git@szab100.com>
2021-10-09 04:16:45 +0000

    externalize env vars

--- .gitpod/ws-init.sh
+++ .gitpod/ws-init.sh
@@ -1,19 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-source ~/.bash_profile
 DOCKER_PSQL_NAME=postgres
+
+set -a
+source ~/.bash_profile
+source .gitpod/.env
 
 # QEMU-K3S init
 source /opt/qemu-k3s/ws-init.sh
+set +a
+
+ln -fs .gitpod/.env ~/.env
+echo "source ~/.env" > ~/.bashrc
 
 # Install Argo WF (one-time)
 .gitpod/install-argo-wf.sh
 
 # Run postgresql
 echo "Running PostgreSQL in a Docker container"
-export PGHOST=127.0.0.1
-export PGPORT=5432
-export PGUSER=root
-export PGPASSWORD=1234
 [[ $(docker ps -f "name=$DOCKER_PSQL_NAME" --format '{{.Names}}') == $DOCKER_PSQL_NAME ]] || docker run --name "$DOCKER_PSQL_NAME" -e POSTGRES_USER=$PGUSER -e POSTGRES_PASSWORD=$PGPASSWORD -d -p 5432:5432 --restart=always postgres
 echo "✅ PSQL setup complete. Use 'psql' and other utilities normally."
